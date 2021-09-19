45852220e08c2896767f1fe47c235ca2b98bc8e5
kolkono <kolkono2@yandex.ru>
2021-09-19 22:28:29 +0300

    Задание 3

---
+++ Pair/Pair/Complex.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+#include "Pair.h"
+class Complex : public Pair {
+private:
+
+	double _real;
+
+	double _imaginary;
+
+public:
+
+	Complex(const double real = 0, const double imaginary = 0)
+		: _real(real)
+		, _imaginary(imaginary)
+	{}
+
+	void operator* (Complex& arg)
+	{
+		_real = _real * arg._real -_imaginary * arg._imaginary;
+		_imaginary = _real * arg._imaginary + _imaginary * arg._real;
+	}
+
+	void operator- (Complex& arg)
+	{
+		_real = _real - _imaginary;
+		_imaginary = arg._real - arg._imaginary;
+	}
+};
