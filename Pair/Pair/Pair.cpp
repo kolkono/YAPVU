90a1f357b33334463f457c0d3e03b9c06ab5e8e4
kolkono <kolkono2@yandex.ru>
2021-09-19 22:35:31 +0300

    Задание 3

---
+++ Pair/Pair/Pair.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include "Pair.h"
+using namespace std;
+
+double Pair::Mul() const
+{
+	return _x * _y;
+}
+
+double Pair::Add(const Pair& other)
+{
+	_x = _x + other._x - (_x + _y);
+	_y = _x + other._y - (other._x + other._y);
+}
