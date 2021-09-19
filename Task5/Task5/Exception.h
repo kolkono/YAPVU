24fee8d6863424d5a9d9d58ef7ce413f8a923d34
kolkono <kolkono2@yandex.ru>
2021-09-19 22:14:35 +0300

    Задание 5

---
+++ Task5/Task5/Exception.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+#include <iostream>
+using namespace std;
+
+class ExeptionEmpty { };
+
+class MyException
+{
+private:
+	string _msg;
+public:
+	MyException(const string& s) : _msg(s) {}
+	const char* message() const { return _msg.c_str(); }
+};
+
+class MyExceptionHier : public invalid_argument
+{
+private:
+	int _num;
+public:
+	MyExceptionHier(const string msg, int n) : invalid_argument(msg), _num(n) {}
+	double arg() const { return _num; }
+};
+
+
+
---
+++ Task5/Task5/Triangle.h
@@ -0,0 +1,66 @@
+﻿#pragma once
+#include "Exception.h"
+using namespace std;
+
+struct Angles
+{
+	double a;
+	double b;
+};
+
+class Triangle
+{
+private:
+	unsigned int _A;
+	unsigned int _B;
+public:
+
+	Triangle(unsigned int A, unsigned int B)
+	{
+
+	}
+
+	~Triangle() = default;
+
+	Angles Calc(unsigned int A, unsigned int B) {
+		Angles ang;
+		ang.a = A / sqrt(pow(A, 2) + pow(B, 2));
+		ang.b = B / sqrt(pow(A, 2) + pow(B, 2));
+		return ang;
+	}
+
+	Angles ConversionNoEx() {
+		return Calc(_A, _B);
+	}
+	Angles ConversionThrow() throw() {
+		return Calc(_A, _B);
+	}
+
+	Angles ConversionSpecification() /* throw(invalid_argument())*/
+	{
+		if (!(_A > 0 && _B > 0))
+			throw invalid_argument("Отрицательный аргумент");
+		return Calc(_A, _B);
+	}
+
+	Angles ConversionEmptyClass() /*throw(ExeptionEmpty)*/
+	{
+		if (!(_A > 0 && _B > 0))
+			throw ExeptionEmpty();
+		return Calc(_A, _B);
+	}
+
+	Angles ConversionMyException() /*throw(MyException)*/
+	{
+		if (!(_A > 0 && _B > 0))
+			throw MyException("Отрицательное время");
+		return Calc(_A, _B);
+	}
+
+	Angles ConversionMyExceptionHier() /*throw(MyExceptionHier)*/
+	{
+		if (!(_A > 0 && _B > 0))
+			throw MyExceptionHier("Отрицательный аргумент", 0);
+		return Calc(_A, _B);
+	}
+};
