8480d88c7cef3d2e30b50806af62da1e9b3d77c3
kolkono <kolkono2@yandex.ru>
2021-09-20 17:03:55 +0300

    Задание 3

---
+++ Pair/Pair/Pair.h
@@ -0,0 +1,52 @@
+﻿#pragma once
+
+class Pair {
+private:
+	/*
+	* \brief x
+	*/
+	double _x;
+	/*
+	* \brief y
+	*/
+	double _y;
+
+public:
+	/*
+	* \brief Конструктор по умолчанию
+	* \param x
+	* \param y
+	*/
+	Pair(const double x = 0, const double y = 0)
+		: _x(x)
+		, _y(y)
+	{}
+	/*
+	* \brief Конструктор копирования по умолчанию
+	*/
+	Pair(const Pair& other) = default;
+	/*
+	* \brief Деструктор
+	*/
+	~Pair() = default;
+
+	/*
+	* \brief Setter x
+	*/
+	void SetX(const double x) { _x = x; }
+	/*
+	* \brief Setter y
+	*/
+	void SetY(const double y) { _y = y; }
+	/*
+	* \brief Getter x
+	*/
+	double GetX() { return _x; }
+	/*
+	* \brief Getter y
+	*/
+	double GetY() { return _y; }
+
+	double Mul() const;
+	double Add(const Pair& other);
+};
